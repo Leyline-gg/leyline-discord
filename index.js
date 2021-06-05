10582ba0096169b994772f06e6c1a0b17fd0a647
elijaholmos <eli.olmos9@gmail.com>
2021-06-05 10:17:28 -0700

    update env var handling

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const { Client, Collection } = require('discord.js');
 const admin = require('firebase-admin');
 const klaw = require('klaw');
 const path = require('path');
-//if (process.env.NODE_ENV !== 'production')    //we use dotenv on prod now
-    require('dotenv').config();
+require('dotenv').config({path: process.env.NODE_ENV === 'production' ? '../.env' : './.env'});
 
 class LeylineBot extends Client {
     discord_log_channel = '843892751276048394'; //for logging actions performed
