b33be40e0ab3a355e8561dab2be857d833ab2c7a
elijaholmos <35435704+elijaholmos@users.noreply.github.com>
2021-06-08 17:35:44 -0700

    prod to staging (#4)
    * dev to prod (#3)
    
    * add Analytics class foundation
    
    * add help command & EmbedBase class
    
    * implement admin command filter
    
    * add public profile url to profile cmd
    
    * implement staging functionality
    
    * fix staging cmd

---
+++ staging-start.sh
@@ -0,0 +1,7 @@
+cd ../staging
+rm -rf leyline-discord/
+git clone -b staging --single-branch git@github.com:Leyline-gg/leyline-discord.git
+cd leyline-discord/
+npm install
+pm2 delete staging
+npm run staging
