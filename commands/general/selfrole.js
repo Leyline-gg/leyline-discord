8a15a28f3b2b313e37b7ea6c1fb05c65b39c2d2b
elijaholmos <eli.olmos9@gmail.com>
2021-06-19 17:36:54 -0700

    cmd(selfrole): create command

---
+++ commands/general/selfrole.js
@@ -0,0 +1,67 @@
+const { Message } = require('discord.js');
+const Command = require('../../classes/Command');
+const EmbedBase = require('../../classes/EmbedBase');
+
+//roles available to be self-roled
+const roles = [
+    '853414453206188063',   //do good alerts
+];
+
+class selfrole extends Command {
+    constructor(bot) {
+        super(bot, {
+            name: 'selfrole',
+            description: 'Give a role to yourself',
+            usage: '[role name]',
+            aliases: [],
+            category: 'general'
+        })
+    }
+
+    /**
+     * 
+     * @param {Message} msg 
+     */
+    async run(msg, args) {
+        try {
+            const bot = this.bot;
+            
+            //obtain and filter Role objects
+            const avail_roles = (await bot.leyline_guild.roles.fetch()).cache.filter(r => roles.includes(r.id));
+
+            //display list of available roles
+            if(args.length < 1) {
+                return msg.channel.send({embed: new EmbedBase(bot, {
+                    fields: [
+                        {
+                            name: 'Available Roles',
+                            value: avail_roles.map(r => r.name).join('\n')
+                        },
+                    ],
+                })});
+            }
+
+            const role = avail_roles.find(r => r.name.toLowerCase() === args.join(' ').toLowerCase());
+            if(!role) return msg.channel.send({embed: new EmbedBase(bot, {
+                    fields: [
+                        {
+                            name: '❌ Role not found',
+                            value: `That role isn't on the list of available roles. To view the list, type \`${bot.config.prefix}${this.name}\``
+                        },
+                    ],
+                }).Error()});
+
+
+            msg.member.roles.add(role, `Self-assigned with the ${this.name} command`)
+                .then(() => msg.react('✅'))
+                .catch(err => {
+                    msg.reply('I ran into an error');
+                    bot.logger.error(err);
+                });
+        } catch (err) {
+            this.bot.logger.error(`${this.name} Error: ${err}`);
+        }
+    }
+}
+
+module.exports = selfrole;
