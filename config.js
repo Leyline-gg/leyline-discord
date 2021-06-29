6af99ec4be2281f4c198a7f722a2e10d1cf615f8
elijaholmos <35435704+elijaholmos@users.noreply.github.com>
2021-06-28 17:23:19 -0700

    Release 1.1.1 (#18)
    * Dev bump to 1.0.0 (#7)
    
    * prod to staging (#4)
    
    * dev to prod (#3)
    
    * add Analytics class foundation
    
    * add help command & EmbedBase class
    
    * implement admin command filter
    
    * add public profile url to profile cmd
    
    * implement staging functionality
    
    * fix staging cmd
    
    * updating staging startup
    
    * update staging startup
    
    * Update index.js
    
    * add staging env variable to start script
    
    * fix shell script err
    
    * force api bypass in staging cmd
    
    * move staging bot to separate VM
    
    * Update README.md
    
    * bump: 1.0.0
    
    * Revert "Merge branch 'dev' into production"
    
    This reverts commit 6042b400dacdf44e70011995adb29f5ed207186d, reversing
    changes made to 3819032d675be8d9e3121e2c317aca03bcfd3a3c.
    
    * create custom DiscordEvent class
    
    * update CURRENT_VERSION handling
    
    * good-acts event foundation
    
    pretty solid start, time for a dinner break with the family
    we're going out to Islands!
    
    * refinements to LeylineBot class
    
    * profile cmd uses `bot.connection_tutorial`
    
    * more progress on good-acts evnt
    
    * implement LeylineBot.checkAdmin()
    
    * light cleanup of index.js
    
    * revise cmd usage documentation
    
    * new admin cmds
    
    -addrole
    -delrole
    -setstatus
    -sudosay
    
    * fat refactor of good-acts evnt
    
    * fix incorrect balance bug with profile cmd
    
    * change awardLLP method to avoid runTransaction
    
    * good-acts: store reactions in custom cache
    
    connected cache to database
    
    * change events dir structure
    
    * big update to good-acts evnt
    
    -code refactor into separate class
    -ReactionCollectors persist across all sessions for both unapproved and approved photos posted by users
    
    * add untested restart cmd
    
    * cmd(help): change usage display
    
    * create Error and Warn embed presets
    
    * evnt(good-acts): any users that reacted before msg was approved get LLP
    
    * cmd(profile): refactor to use EmbedBase
    
    - error messages now display red as well
    
    * evnt(good-acts): change cache order
    
    - users that have already reacted are cached immediately, to match the approval procedure
    
    * cmd(selfrole): create command
    
    * prep for staging
    
    * small code cleanup
    
    * 1.1.0 dev to staging, draft 2 (rewritten) (#10)
    
    * cmd(addrole): fix an err response bug
    
    * implement bot.sendDisabledDmMessage
    
    * 1.1.0 dev to staging, draft 3 (#11)
    
    * cmd(addrole): fix an err response bug
    
    * implement bot.sendDisabledDmMessage
    
    * changes suggested at community sync
    
    * 1.1.0 dev to staging, hotfix (#13)
    
    * cmd(addrole): fix an err response bug
    
    * implement bot.sendDisabledDmMessage
    
    * changes suggested at community sync
    
    * hotfix(good-acts): update target_channel
    
    * 1.1.1 dev to staging (#17)
    
    * cmd(addrole): fix an err response bug
    
    * implement bot.sendDisabledDmMessage
    
    * changes suggested at community sync
    
    * hotfix(good-acts): update target_channel
    
    * Release 1.1.0 - hotfix (#14)
    
    * 1.1.0 dev to staging, draft 3 (#11)
    
    * cmd(addrole): fix an err response bug
    
    * implement bot.sendDisabledDmMessage
    
    * changes suggested at community sync
    
    * 1.1.0 dev to staging, hotfix (#13)
    
    * cmd(addrole): fix an err response bug
    
    * implement bot.sendDisabledDmMessage
    
    * changes suggested at community sync
    
    * hotfix(good-acts): update target_channel
    
    * refactor to accommodate new Dev discord
    
    * event(good-acts): New reactions for different activities
    
    * update gitignore & log errors better
    
    * evnt(good-acts): various revisions, see desc
    
    -added 2 new reactions
    -refactored order of events to hopefully flow better from a user POV

--- config.js
+++ config.js
@@ -2,11 +2,26 @@
 module.exports = {
     production: {
         prefix: '!',
+        leyline_guild_id: '751913089271726160',
+        //private logs
+        discord_log_channel: '843892751276048394',
+        //public logs
+        discord_bot_channel: '810265135419490314',
     },
     development: {
         prefix: '!!',
+        leyline_guild_id: '857839180608307210',
+        //private logs
+        discord_log_channel: '858141871788392448',
+        //public logs
+        discord_bot_channel: '858141914841481246',
     },
     staging: {
         prefix: '$',
+        leyline_guild_id: '857839180608307210',
+        //private logs
+        discord_log_channel: '858141871788392448',
+        //public logs
+        discord_bot_channel: '858141914841481246',
     },
 }
