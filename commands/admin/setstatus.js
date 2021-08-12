e900424e24f6b428da8a35eca209c1a86c85dd8a
elijaholmos <eli.olmos9@gmail.com>
2021-08-11 21:54:36 -0700

    cmd(setstatus): finish refactor
    changes:
    -status is no longer reset by running cmd without args; "clear" needs to be passed as text now

--- commands/admin/setstatus.js
+++ commands/admin/setstatus.js
@@ -1,21 +1,53 @@
 const { ActivityTypes } = require('discord.js').Constants;
 const Command = require('../../classes/Command');
+const EmbedBase = require('../../classes/EmbedBase');
 
 class setstatus extends Command {
     constructor(bot) {
         super(bot, {
             name: 'setstatus',
             description: 'Set the Discord status of the bot',
-            //TODO: fix this cmd
+            options: [
+                {
+                    type: 'STRING',
+                    name: 'activity-type',
+                    description: 'The type of activity to display',
+                    required: true,
+                    choices: Object.keys(ActivityTypes)
+                        //we don't want the numbers for the UI & bots can't set CUSTOM activities
+                        .filter(a => !isNaN(Number(ActivityTypes[a])) && a !== 'CUSTOM')
+                        .map(a => ({
+                            name: a.charAt(0) + a.substring(1).toLowerCase(),
+                            value: a,
+                    })),
+                },
+                {
+                    type: 'STRING',
+                    name: 'text',
+                    description: 'The status text to display. Enter "clear" to remove the current status',
+                    required: true,
+                },
+                
+            ],
             aliases: ['setactivity', 'sa'],
             category: 'admin',
         });
     }
 
     run({intr, opts}) {
-        if(args.length < 1) return this.bot.user.setPresence({activty: null}).then(() => msg.reply('Status cleared'));
-        const type = ActivityTypes.includes(args[0].toUpperCase()) ? args.shift().toUpperCase() : 'PLAYING';
-        this.bot.user.setActivity(args.join(' '), { type: type });
+        const bot = this.bot;
+        const status_name = opts.getString('text');
+
+        bot.user.setPresence({activities:[
+            ...(status_name !== 'clear' ? [{
+                name: status_name,
+                type: ActivityTypes[opts.getString('activity-type')],
+            }] : []), 
+        ]});
+
+        bot.intrReply({intr, embed: new EmbedBase(bot, {
+            description: `✅ **Status updated**`,
+        }).Success()});
     }
 }
 
