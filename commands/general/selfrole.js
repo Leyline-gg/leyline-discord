3b3ad6c5dfc6fdb4f3fad0bae9db40839b003697
elijaholmos <eli.olmos9@gmail.com>
2021-08-13 18:35:09 -0700

    Squashed commit of the following:
    commit 26a8650058baec853f9cde738769ede174c406f4
    Author: elijaholmos <eli.olmos9@gmail.com>
    Date:   Wed Aug 11 15:41:53 2021 -0700
    
        committing changes made to awardnft cmd
    
    commit eca1b518f79ad1c7600ac808c6844be812305d82
    Author: elijaholmos <eli.olmos9@gmail.com>
    Date:   Wed Aug 11 15:36:10 2021 -0700
    
        Create ConfirmInteraction.js
    
    commit fe914fff9273fc49a6dd99927a203a89bf13e97a
    Author: elijaholmos <eli.olmos9@gmail.com>
    Date:   Wed Aug 11 15:36:00 2021 -0700
    
        messing with something that already works fine

--- commands/general/selfrole.js
+++ commands/general/selfrole.js
@@ -1,64 +1,85 @@
-const { Message } = require('discord.js');
 const Command = require('../../classes/Command');
 const EmbedBase = require('../../classes/EmbedBase');
 
 //roles available to be self-roled
 const roles = [
-    '853414453206188063',   //do good alerts
+    //'853414453206188063',   //do good alerts
+    //'874704370103615559',   //Bot Updates
+    '873234204706631720',
+    '865629785015320608',
 ];
 
 class selfrole extends Command {
     constructor(bot) {
         super(bot, {
             name: 'selfrole',
-            description: 'Give/take roles to/from yourself',
+            description: 'Give a role to yourself',
+            usage: '[role name]',
             aliases: [],
             category: 'general'
         });
     }
-
-    /**
-     * 
-     * @param {Message} msg 
-     */
-    async run({intr, opts}) {
+    
+    async run(msg, args) {
         try {
             const bot = this.bot;
             
             //obtain and filter Role objects
             const avail_roles = (await bot.leyline_guild.roles.fetch()).filter(r => roles.includes(r.id));
 
-            //display list of available roles
-            if(args.length < 1) {
-                return bot.intrReply({intr, embed: new EmbedBase(bot, {
-                    fields: [
-                        {
-                            name: 'Available Roles',
-                            value: avail_roles.map(r => r.name).join('\n'),
-                        },
-                    ],
-                })});
-            }
-
-            const role = avail_roles.find(r => r.name.toLowerCase() === args.join(' ').toLowerCase());
-            if(!role) return bot.intrReply({intr, embed: new EmbedBase(bot, {
-                    fields: [
-                        {
-                            name: '❌ Role not found',
-                            value: `That role isn't on the list of available roles. To view the list, type \`${bot.config.prefix}${this.name}\``
-                        },
-                    ],
-                }).Error()});
-
-
-            msg.member.roles.add(role, `Self-assigned with the ${this.name} command`)
-                .then(() => msg.react('✅'))
-                .catch(err => {
-                    msg.reply('I ran into an error');
-                    bot.logger.error(err);
-                });
+            const m = {
+                components: [
+                    {
+                        custom_id: 'role-menu',
+                        disabled: false,
+                        placeholder: 'Select a role...',
+                        min_values: 1,
+                        options: avail_roles.map(r => {return {
+                            label: r.name,
+                            value: r.id,
+                        }}),
+                        type: 3
+                    }
+                ],
+                type: 1
+            };
+            
+            return this.awaitInteraction({
+                msg: await msg.channel.send({
+                    content: `Choose from the below list. Roles you don't already have will be added to you, and roles you do have will be removed.`,
+                    components: [m],
+                }),
+                cb: this.interactionResponse.bind(this),    //funky syntax reason: https://stackoverflow.com/a/59060545/8396479
+            });
         } catch (err) {
             this.bot.logger.error(`${this.name} Error: ${err}`);
+        }
+    }
+
+    async interactionResponse(i) {
+        const bot = this.bot;
+        try {
+            const [add, rem] = [[], []];
+            //filter roles so we can reference them later
+            for(const role of i.values) 
+                i.member.roles.cache.has(role) ? rem.push(role) : add.push(role);
+            await i.member.roles.add(add, `Self-assigned with the ${this.name} command`);
+            await i.member.roles.remove(rem, `Self-removed with the ${this.name} command`);
+
+            i.update({embeds: [new EmbedBase(bot, {
+                fields: [
+                    ...(!!add.length ? [{
+                        name: 'Roles Added',
+                        value: add.map(role => bot.leyline_guild.roles.resolve(role).toString()).join('\n'),
+                    }] : []),
+                    ...(!!rem.length ? [{
+                        name: 'Roles Removed',
+                        value: rem.map(role => bot.leyline_guild.roles.resolve(role).toString()).join('\n'),
+                    }] : []),
+                ],
+            })], components: [], content: '\u200b',})
+        } catch (err) {
+            bot.logger.error(`${this.name} Error: ${err}`);
         }
     }
 }
