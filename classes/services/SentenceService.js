da24ffff2c3843044cea66e1124a272c16c8c17a
elijaholmos <35435704+elijaholmos@users.noreply.github.com>
2021-10-25 17:20:54 -0700

    Split justice commands (#110)

--- classes/services/SentenceService.js
+++ classes/services/SentenceService.js
@@ -51,8 +51,8 @@ export class SentenceService {
      * @param {number} [args.timestamp] Unix timestamp of when sentence was issued. Defaults to `Date.now()`
      * @returns Resolves to added doc
      */
-    static async logSentence({bot, user, mod, type, expires=null, reason=null, timestamp=Date.now()} = {}) {
-        const { SENTENCE_TYPES: SENTENCE_TYPES } = this;
+    static async logSentence({bot, user, mod, sentence_type: type, expires=null, reason=null, timestamp=Date.now()} = {}) {
+        const { SENTENCE_TYPES } = this;
         const embed = new EmbedBase(bot, {
             title: 'Justice Served',
             fields: [
---
+++ commands/admin/justice/warn.js
@@ -0,0 +1,67 @@
+import { JusticeCommand, SentenceService, EmbedBase } from '../../../classes';
+
+class warn extends JusticeCommand {
+    constructor(bot) {
+        super(bot, {
+            name: 'warn',
+            sentence_type: SentenceService.SENTENCE_TYPES.WARN,
+            description: 'Issue a written warning to a Discord user',
+            options: {
+                duration: false,
+            },
+        });
+    }
+
+    //Override parent
+    async executeSentence({intr, user, reason}) {
+        const { bot, sentence_type } = this;
+        //issue sentence
+        await SentenceService.warnUser({
+            bot,
+            user,
+            mod: intr.user,
+            reason,
+        });
+        //log sentence
+        /*await*/ SentenceService.logSentence({
+            bot,
+            user,
+            mod: intr.user,
+            sentence_type,
+            reason,
+        });
+        return bot.intrReply({intr, embed: new EmbedBase(bot, {
+            description: `⚖ **Sentence Successfully Issued**`,
+        }).Sentence(), ephemeral: true});
+    }
+
+    async run({intr, opts}) {
+        const { bot, sentence_type } = this;
+        const { SENTENCE_TYPES } = SentenceService;
+
+        const { user, reason } = super.parseInput(opts);
+
+        //send confirm prompt if this is a sentence in SENTENCE_TYPES
+        if (Object.keys(SENTENCE_TYPES).includes(sentence_type))
+            if (!(await super.getModConfirmation({intr, user, reason})))
+                return bot.intrReply({
+                    intr,
+                    ephemeral: true,
+                    embed: new EmbedBase(bot, {
+                        description: `❌ **Sentence canceled**`,
+                    }).Error(),
+                });
+
+
+        //easter egg
+        if(!!super.checkEasterEgg({intr, user})) return;
+
+        this.executeSentence({intr, user, reason})
+            .catch(err => {
+                bot.logger.error(err);
+                bot.intrReply({intr, embed: new EmbedBase(bot).ErrorDesc(err.message), ephemeral: true});
+            });
+    }
+}
+
+export default warn;
