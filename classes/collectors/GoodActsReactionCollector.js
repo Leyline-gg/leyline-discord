7fa1139ffe0fc5c031b51ab270e2a052689ff707
elijaholmos <eli.olmos9@gmail.com>
2021-10-29 12:44:08 -0700

    change LLP to GP in GoodActsReactionCollector

--- classes/collectors/GoodActsReactionCollector.js
+++ classes/collectors/GoodActsReactionCollector.js
@@ -5,8 +5,8 @@ const CTA_ROLE 			= '853414453206188063'; //role to ping when photo is approved
 
 export class GoodActsReactionCollector extends ReactionCollectorBase {
 	//override parent properties
-	get REACTION_LLP() { return CloudConfig.get('ReactionCollector').GoodActs.REACTION_LLP; }
-	get APPROVAL_LLP() { return CloudConfig.get('ReactionCollector').GoodActs.APPROVAL_LLP; }
+	get REACTION_GP() { return CloudConfig.get('ReactionCollector').GoodActs.REACTION_GP; }
+	get APPROVAL_GP() { return CloudConfig.get('ReactionCollector').GoodActs.APPROVAL_GP; }
 	get MOD_EMOJIS() { return CloudConfig.get('ReactionCollector').GoodActs.MOD_EMOJIS; }
 
 	media_placeholder	//unfortunately, there is no easy way to extract the thumbnail from a video posted in discord
@@ -29,14 +29,14 @@ export class GoodActsReactionCollector extends ReactionCollectorBase {
 
 		//check if user who reacted is msg author
 		if(user.id === msg.author.id) return;
-		//award LLP to msg author
+		//award GP to msg author
 		if(!(await Firebase.isUserConnectedToLeyline(msg.author.id))) 
 			this.handleUnconnectedAccount(msg.author, {
-				dm: `Your [${this.media_type}](${msg.url} 'click to view message') posted in <#${msg.channel.id}> received a reaction, but because you have not connected your Discord & Leyline accounts, I couldn't award you any LLP!
+				dm: `Your [${this.media_type}](${msg.url} 'click to view message') posted in <#${msg.channel.id}> received a reaction, but because you have not connected your Discord & Leyline accounts, I couldn't award you any GP!
 					[Click here](${bot.connection_tutorial} 'How to connect your accounts') to view the account connection tutorial`,
-				log: `${bot.formatUser(msg.author)}'s [${this.media_type}](${msg.url} 'click to view message') posted in <#${msg.channel.id}> recevied a reaction, but I did not award them any LLP because they have not connected their Leyline & Discord accounts`,
+				log: `${bot.formatUser(msg.author)}'s [${this.media_type}](${msg.url} 'click to view message') posted in <#${msg.channel.id}> recevied a reaction, but I did not award them any GP because they have not connected their Leyline & Discord accounts`,
 			});
-		else await this.awardAuthorReactionLLP({
+		else await this.awardAuthorReactionGP({
 			user: user,
 			pog: `Discord <a href="${msg.url}">\
 				${msg._activityType} ${this.media_type[0].toUpperCase() + this.media_type.slice(1)}</a> Received Reaction`,
@@ -47,7 +47,7 @@ export class GoodActsReactionCollector extends ReactionCollectorBase {
 	async approveSubmission({user, reaction}) {
 		const { bot, msg } = this;
 		try {
-			//store the activity type for LLP award text both locally and in the cloud
+			//store the activity type for GP award text both locally and in the cloud
 			msg._activityType = this.MOD_EMOJIS.find(e => e.emoji_id === reaction.emoji.toString())?.keyword || 'Good Act';
 			await Firebase.approveCollector({collector: this, user, metadata: {
 				activity_type: msg._activityType,
@@ -64,7 +64,7 @@ export class GoodActsReactionCollector extends ReactionCollectorBase {
 				msg,
 				content: `<@&${CTA_ROLE}> 🚨 **NEW APPROVED ${this.media_type.toUpperCase()}!!** 🚨`,
 				embed: new EmbedBase(bot, {
-					description: `A new ${this.media_type} was approved! Click [here](${msg.url} 'view message') to view the message.\nBe sure to react within 24 hours to get your LLP!`,
+					description: `A new ${this.media_type} was approved! Click [here](${msg.url} 'view message') to view the message.\nBe sure to react within 24 hours to get your GP!`,
 					thumbnail: { url: this.media_type === 'photo' ? msg.attachments.first().url : this.media_placeholder },
 				}),
 			});
@@ -106,21 +106,21 @@ export class GoodActsReactionCollector extends ReactionCollectorBase {
 			const is_author_connected = await Firebase.isUserConnectedToLeyline(msg.author.id);
 			if(!is_author_connected)
 				this.handleUnconnectedAccount(msg.author, {
-					dm: `Your [${this.media_type}](${msg.url} 'click to view message') posted in <#${msg.channel.id}> was approved, but because you have not connected your Discord & Leyline accounts, I couldn't award you any LLP!
+					dm: `Your [${this.media_type}](${msg.url} 'click to view message') posted in <#${msg.channel.id}> was approved, but because you have not connected your Discord & Leyline accounts, I couldn't award you any GP!
 						[Click here](${bot.connection_tutorial} 'How to connect your accounts') to view the account connection tutorial`,
-					log: `${bot.formatUser(msg.author)}'s [${this.media_type}](${msg.url} 'click to view message') posted in <#${msg.channel.id}> was approved, but I did not award them any LLP because they have not connected their Leyline & Discord accounts`,
+					log: `${bot.formatUser(msg.author)}'s [${this.media_type}](${msg.url} 'click to view message') posted in <#${msg.channel.id}> was approved, but I did not award them any GP because they have not connected their Leyline & Discord accounts`,
 				});
 
-			// I could add some catch statements here and log them to Discord (for the awarding LLP process)
+			// I could add some catch statements here and log them to Discord (for the awarding GP process)
 
-			//award LLP to msg author
-			else await this.awardApprovalLLP({
+			//award GP to msg author
+			else await this.awardApprovalGP({
 				user: msg.author,
 				pog: `Discord <a href="${msg.url}">\
 					${msg._activityType} ${this.media_type[0].toUpperCase() + this.media_type.slice(1)}</a> Approved`,
 			});
 
-			// ---  Give LLP to the users that have already reacted   ---
+			// ---  Give GP to the users that have already reacted   ---
 			// --- (this includes the mod that just approved the msg) ---
 			await msg.fetchReactions();
 			for(const old_reaction of [...msg.reactions.cache.values()]) {
@@ -129,11 +129,11 @@ export class GoodActsReactionCollector extends ReactionCollectorBase {
 						//store user's reaction right away, because we do the same in the approved collector
 						await this.storeUserReaction(old_user);
 
-						//award LLP to msg author for receiving a reaction (except on his own reaction)
+						//award GP to msg author for receiving a reaction (except on his own reaction)
 						//(this goes above the continue statement below)
 						is_author_connected &&
 							old_user.id !== msg.author.id &&
-							await this.awardAuthorReactionLLP({
+							await this.awardAuthorReactionGP({
 								user: old_user,
 								pog: `Discord <a href="${msg.url}">\
 									${msg._activityType} ${this.media_type[0].toUpperCase() + this.media_type.slice(1)}</a> Received Reaction`,
@@ -142,15 +142,15 @@ export class GoodActsReactionCollector extends ReactionCollectorBase {
 						//exit if user is not connected to Leyline
 						if(!(await Firebase.isUserConnectedToLeyline(old_user.id))) {
 							this.handleUnconnectedAccount(old_user, {
-								dm: `You reacted to the [${this.media_type}](${msg.url} 'click to view message') posted by ${bot.formatUser(msg.author)} in <#${msg.channel.id}>, but because you have not connected your Discord & Leyline accounts, I couldn't award you any LLP!
+								dm: `You reacted to the [${this.media_type}](${msg.url} 'click to view message') posted by ${bot.formatUser(msg.author)} in <#${msg.channel.id}>, but because you have not connected your Discord & Leyline accounts, I couldn't award you any GP!
 									[Click here](${bot.connection_tutorial} 'How to connect your accounts') to view the account connection tutorial`,
-								log: `${bot.formatUser(old_user)} reacted to the [${this.media_type}](${msg.url} 'click to view message') posted by ${bot.formatUser(msg.author)} in <#${msg.channel.id}>, but I did not award them any LLP because they have not connected their Leyline & Discord accounts`,
+								log: `${bot.formatUser(old_user)} reacted to the [${this.media_type}](${msg.url} 'click to view message') posted by ${bot.formatUser(msg.author)} in <#${msg.channel.id}>, but I did not award them any GP because they have not connected their Leyline & Discord accounts`,
 							});
 							continue;
 						}
 
-						//award LLP!
-						await this.awardReactionLLP({user: old_user});
+						//award GP!
+						await this.awardReactionGP({user: old_user});
 					}
 				}
 			}
